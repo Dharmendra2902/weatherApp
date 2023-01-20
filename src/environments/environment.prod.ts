8a651a74db1888c41fdc3ccba6b0c8a11343221c
Dharmendra Gorana <dharmendra.gorana.sbg@gmail.com>
2023-01-20 12:19:22 +0530

    production changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/forecast/',
+  appid: 'c51223c219d6aec8cb8c5210449bd859'
 };
